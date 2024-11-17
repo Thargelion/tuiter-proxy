a76a9edc2b5b9d9f24e7301c1475732d7df6da25
Maximiliano De Pietro <elwindwalker@gmail.com>
2024-11-17 15:43:55 -0300

    :green_hearth: fix scripcito

--- lock-and-load.sh
+++ lock-and-load.sh
@@ -3,8 +3,8 @@
 current_dir=$(pwd)
 ftp_dir="/home10/ddboammf/tuiter.fragua.com.ar/public_html"
 
-sed -i '' "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/config.php
-sed -i '' "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/packages.php
-sed -i '' "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/events.php
-sed -i '' "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/services.php
-sed -i '' "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/routes-v7.php
+sed -i "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/config.php
+sed -i "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/packages.php
+sed -i "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/events.php
+sed -i "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/services.php
+sed -i "s@$current_dir@$ftp_dir@g" ./bootstrap/cache/routes-v7.php
